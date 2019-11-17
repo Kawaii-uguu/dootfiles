734add74124b34c5eae7e45d44fa5e5b6ce6f8a5
Kawaii-uguuisstupid <Kawaii-uguu@github.com>
2019-11-16 18:15:55 -0800

    umu

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,6 @@
+/home/youare/Music/sdflskjdnhglakjsdhglkajdfhglkasdjhglsdkujhawloignhs.d,mf bnkd,fjhvnbldsunskj/Camellia - When you say Hai this song is gonna be go insanely fast.mp3
+/home/youare/Music/sdflskjdnhglakjsdhglkajdfhglkasdjhglsdkujhawloignhs.d,mf bnkd,fjhvnbldsunskj/Mitchie M - 歌の棲む家メゾン初音 feat 初音ミク.mp3
+/home/youare/Music/sdflskjdnhglakjsdhglkajdfhglkasdjhglsdkujhawloignhs.d,mf bnkd,fjhvnbldsunskj/TORIENA - NOT IDOL.mp3
+/home/youare/Music/sdflskjdnhglakjsdhglkajdfhglkasdjhglsdkujhawloignhs.d,mf bnkd,fjhvnbldsunskj/tpazolite - cheatreal.mp3
+/home/youare/Music/sdflskjdnhglakjsdhglkajdfhglkasdjhglsdkujhawloignhs.d,mf bnkd,fjhvnbldsunskj/gigaP - Gigantic OTN.mp3
+/home/youare/Music/sdflskjdnhglakjsdhglkajdfhglkasdjhglsdkujhawloignhs.d,mf bnkd,fjhvnbldsunskj/YUC'e - Future Cαndy.mp3
